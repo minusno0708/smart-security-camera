d7d0d5f695c1701504d3c7c62eb2a0c6e50c763f
minusno0708 <naoyao1014@gmail.com>
2023-09-17 15:35:56 +0900

    feat:watchページでsocketに接続

--- client/pages/watcher.vue
+++ client/pages/watcher.vue
@@ -4,3 +4,41 @@
         <NuxtLink to="/">Home</NuxtLink>
     </div>
 </template>
+
+
+<script>
+export default {
+    name: 'VideoWatching',
+    data() {
+        return {
+            ws: null,
+        }
+    },
+    mounted() {
+        this.setupWebSocket();
+    },
+    methods: {
+        setupWebSocket() {
+            this.ws = new WebSocket("ws://localhost:4000/socket/websocket");
+            this.ws.onopen = () => {
+                console.log("WebSocketに接続しました。");
+
+                const request = {
+                    topic: "camera:lobby",
+                    ref: 1,
+                    payload: {
+                    },
+                    event: "phx_join"
+                };
+                this.ws.send(JSON.stringify(request));
+            };
+            this.ws.onmessage = event => {
+                console.log("メッセージを受信しました。", event.data);
+            };
+            this.ws.onclose = () => {
+                console.log("WebSocketを切断しました。");
+            };
+        },
+    }
+}
+</script>
