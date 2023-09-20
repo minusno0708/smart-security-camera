07953fcf6531cbf8f7d2317ab984c059c895ca4b
minusno0708 <naoyao1014@gmail.com>
2023-09-20 16:42:09 +0900

    refactor:camera.vueをscript setup形式で記述する

--- client/pages/camera.vue
+++ client/pages/camera.vue
@@ -7,85 +7,83 @@
     </div>
 </template>
 
-<script>
-export default {
-    name: 'CameraStreaming',
-    data() {
-        return {
-            video: null,
-            ws: null,
-        }
-    },
-    mounted() {
-        this.setupCamera();
-        this.setupWebSocket();
-        this.sendFrame();
-    },
-    methods: {
-        setupCamera() {
-            this.video = this.$refs.videoRef;
-            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia({ 
-                    video: true,
-                    audio: false,
-                 }).then(stream => {
-                    this.video.srcObject = stream;
-                    this.video.play();
-                }).catch(error => {
-                    console.error("カメラにアクセスできませんでした:", error);
-                });
-            } else {
-                console.warn("お使いのブラウザにサポートされていません。");
-            }
-        },
-        setupWebSocket() {
-            this.ws = new WebSocket("ws://localhost:4000/socket/websocket");
-            this.ws.onopen = () => {
-                console.log("WebSocketに接続しました。");
+<script setup>
+const video = ref(null);
+const ws = ref(null);
 
-                const request = {
-                    topic: "camera:123",
-                    ref: 1,
-                    payload: {
-                    },
-                    event: "phx_join"
-                };
-                this.ws.send(JSON.stringify(request));
-            };
-            this.ws.onmessage = event => {
-                console.log("メッセージを受信しました。", event.data);
-            };
-            this.ws.onclose = () => {
-                console.log("WebSocketを切断しました。");
-            };
-        },
-        sendFrame() {
-            const canvas = this.$refs.canvasRef;
-            this.video.addEventListener('loadedmetadata', () => {
-                canvas.width = this.video.videoWidth;
-                canvas.height = this.video.videoHeight;
-            });
+const videoRef = ref(null);
+const canvasRef = ref(null);
 
-            const ctx = canvas.getContext('2d');
+onMounted(() => {
+    setupCamera();
+    setupWebSocket();
+    sendFrame();
+});
 
-            setInterval(() => {   
-                ctx.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
-                const frameData = canvas.toDataURL('image/jpeg');
-
-                const request = {
-                    topic: "camera:123",
-                    ref: 1,
-                    payload: {
-                        message: frameData
-                    },
-                    event: "shout"
-                };
-
-                if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-                    this.ws.send(JSON.stringify(request));
-                }
-            }, 100);
-        },
+const setupCamera = () => {
+    video.value = videoRef.value;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ 
+            video: true,
+            audio: false,
+            }).then(stream => {
+            video.value.srcObject = stream;
+            video.value.play();
+        }).catch(error => {
+            console.error("カメラにアクセスできませんでした:", error);
+        });
+    } else {
+        console.warn("お使いのブラウザにサポートされていません。");
     }
+}
+
+const setupWebSocket = () => {
+    ws.value = new WebSocket("ws://localhost:4000/socket/websocket");
+    ws.value.onopen = () => {
+        console.log("WebSocketに接続しました。");
+
+        const request = {
+            topic: "camera:123",
+            ref: 1,
+            payload: {
+            },
+            event: "phx_join"
+        };
+        ws.value.send(JSON.stringify(request));
+    };
+    ws.value.onmessage = event => {
+        console.log("メッセージを受信しました。", event.data);
+    };
+    ws.value.onclose = () => {
+        console.log("WebSocketを切断しました。");
+    };
+}
+
+const sendFrame = () => {
+    const canvas = canvasRef.value;
+    video.value.addEventListener('loadedmetadata', () => {
+        canvas.width = video.value.videoWidth;
+        canvas.height = video.value.videoHeight;
+    });
+
+    const ctx = canvas.getContext('2d');
+
+    setInterval(() => {   
+        ctx.drawImage(video.value, 0, 0, video.value.videoWidth, video.value.videoHeight);
+        const frameData = canvas.toDataURL('image/jpeg');
+
+        const request = {
+            topic: "camera:123",
+            ref: 1,
+            payload: {
+                message: frameData
+            },
+            event: "shout"
+        };
+
+        if (ws.value && ws.value.readyState === WebSocket.OPEN) {
+            ws.value.send(JSON.stringify(request));
+        }
+    }, 100);
 }
 </script>
