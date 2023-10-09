afb9e36a795ef3705438ce0a106aee6942a019c1
minusno0708 <naoyao1014@gmail.com>
2023-10-09 15:13:58 +0900

    feat:dev pageからsocketに接続

--- client/pages/dev/connect_api.vue
+++ client/pages/dev/connect_api.vue
@@ -1,7 +1,37 @@
 <template>
     <h1>API Connection</h1>
+    <p>Message: {{ message }}</p>
 </template>
 
 <script setup>
 
+const ws = ref(null);
+const message = ref("");
+
+onMounted(() => {
+    setupWebSocket();
+});
+
+const setupWebSocket = () => {
+    ws.value = new WebSocket("ws://localhost:8000/ws");
+    ws.value.onopen = () => {
+        console.log("WebSocketに接続しました。");
+
+        const request = "Hello, Detecto API";
+
+        ws.value.send(JSON.stringify(request));
+    };
+    ws.value.onmessage = event => {
+        console.log("メッセージを受信しました。", event.data);
+
+        try {
+          message.value = event.data;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    ws.value.onclose = () => {
+        console.log("WebSocketを切断しました。");
+    };
+}
 </script>
